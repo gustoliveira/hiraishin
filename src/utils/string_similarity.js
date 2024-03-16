5c6b003f88a40087f916ff7425870aadf571ab1d
Gustavo de Oliveira <gust.oliveira.ferreira@gmail.com>
2024-03-16 15:24:21 -0300

    Add dice algorithm to a class

--- src/utils/string_similarity.js
+++ src/utils/string_similarity.js
@@ -1,49 +1,46 @@
-function compareValues(first, second) {
-  first = first.replace(/\s+/g, '');
-  second = second.replace(/\s+/g, '');
+export class DiceAlgorithm {
+  static compareValues(first, second) {
+    first = first.replace(/\s+/g, '');
+    second = second.replace(/\s+/g, '');
 
-  if (first === second) return 1;
-  if (first.length < 2 || second.length < 2) return 0;
+    if (first === second) return 1;
+    if (first.length < 2 || second.length < 2) return 0;
 
-  let firstBigrams = {};
-  for (let index = 0; index < first.length - 1; index++) {
-    const bigram = first.substring(index, index + 2);
-    const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram) + 1 : 1;
-    firstBigrams.set(bigram, count);
-  }
-
-  let intersectionSize = 0;
-  for (let index = 0; index < second.length - 1; index++) {
-    const bigram = second.substring(index, index + 2);
-    const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram) : 0;
-
-    if (count > 0) {
-      firstBigrams.set(bigram, count - 1);
-      intersectionSize++;
+    let firstBigrams = new Map();
+    for (let index = 0; index < first.length - 1; index++) {
+      const bigram = first.substring(index, index + 2);
+      const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram) + 1 : 1;
+      firstBigrams.set(bigram, count);
     }
-  }
 
-  return (2.0 * intersectionSize) / (first.length + second.length - 2);
-}
+    let intersectionSize = 0;
+    for (let index = 0; index < second.length - 1; index++) {
+      const bigram = second.substring(index, index + 2);
+      const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram) : 0;
 
-function bestMatch(mainString, targetStrings) {
-  const ratings = [];
-  let bestMatchIndex = 0;
-
-  for (let index = 0; index < targetStrings.length; index++) {
-    const currentTargetString = targetStrings[index];
-    const currentRating = compareValues(mainString, currentTargetString);
-    ratings.push({ target: currentTargetString, rating: currentRating });
-
-    if (currentRating > ratings[bestMatchIndex].rating) {
-      bestMatchIndex = index;
+      if (count > 0) {
+        firstBigrams.set(bigram, count - 1);
+        intersectionSize++;
+      }
     }
+
+    return (2.0 * intersectionSize) / (first.length + second.length - 2);
   }
 
-  return { ratings: ratings, bestMatch: ratings[bestMatchIndex], bestMatchIndex: bestMatchIndex };
-}
+  static bestMatch(mainString, targetStrings) {
+    const ratings = [];
+    let bestMatchIndex = 0;
 
-module.exports = {
-  compareValues: compareValues,
-  bestMatch: bestMatch,
-};
+    for (let index = 0; index < targetStrings.length; index++) {
+      const currentTargetString = targetStrings[index];
+      const currentRating = DiceAlgorithm.compareValues(mainString, currentTargetString);
+      ratings.push({ target: currentTargetString, rating: currentRating });
+
+      if (currentRating > ratings[bestMatchIndex].rating) {
+        bestMatchIndex = index;
+      }
+    }
+
+    return { ratings: ratings, bestMatch: ratings[bestMatchIndex], bestMatchIndex: bestMatchIndex };
+  }
+}
